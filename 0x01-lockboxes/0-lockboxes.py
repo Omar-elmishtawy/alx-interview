0171b60f0997182eb2fd3f44cf9a85272bb49624
root <omar.elmishtawy@gmail.com>
2024-06-14 11:06:27 -0700

    Canunlock all

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This is checking if all the boxes can be opened
+"""
+
+def canUnlockAll(boxes):
+
+    def get_key(box):
+        for key in box:
+            keys.add(key)
+
+    opened = {0}
+    keys = set(boxes[0])
+
+    while len(keys):
+        key = keys.pop()
+        if key not in opened:
+            opened.add(key)
+            get_key(boxes[key])
+
+
+    return len(opened) == len(boxes)
